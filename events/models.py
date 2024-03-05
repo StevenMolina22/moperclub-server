df94483c401fc08994d1cf25b7f7b91cd6677420
Steven Molina <steven.molina.slayer@gmail.com>
2024-03-05 17:19:06 -0300

    collecstatic new setting

--- events/models.py
+++ events/models.py
@@ -12,6 +12,6 @@ class Event(models.Model):
   website = models.URLField(null=True, blank=True)
   phone_number = models.CharField(max_length=255, null=True, blank=True)
   # email = models.ForeignKey
-  image = models.ImageField(upload_to="media", null=True, blank=True)
+  image = models.ImageField(upload_to="", null=True, blank=True)
   def __str__(self):
     return self.name
