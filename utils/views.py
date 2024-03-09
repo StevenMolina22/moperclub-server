1b7e963bd0cc4b383f71093a2581ba991a52fe25
Steven Molina <steven.molina.slayer@gmail.com>
2024-03-09 02:12:31 -0300

    featured settings

--- utils/views.py
+++ utils/views.py
@@ -3,7 +3,9 @@ from django.http import JsonResponse
 from django.contrib.sites.shortcuts import get_current_site  # Import get_current_site
 from .utils import get_featured_instances
 
-def get_featured_data(request):  
+def get_featured_data(request): 
+    server_hosted = "http://moperclub-server-v2.vercel.app.com" 
+    server_local = "http://127.0.0.1:8000"
     featured_instances = get_featured_instances()
 
     # serialize each instance into a dictionary
@@ -14,7 +16,8 @@ def get_featured_data(request):
           'name': instance.name,
           'description': instance.description,
           'address': instance.address,
-          'image': f"http://127.0.0.1:8000{instance.image.url}"  if instance.image else "",  # Get the URL of the image file
+          'image': f"{server_hosted}{instance.image.url}"  if instance.image else "",  # Get the URL of the img file dist
+          # 'image': f"{server_local}{instance.image.url}"  if instance.image else "",  # Get the URL of the img file dev
       }
       serialized_instances.append(seriealized_instance)
 
