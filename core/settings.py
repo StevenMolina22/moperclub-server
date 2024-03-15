c279f2f9e2effbdd8bff45f8368de088381a486f
Steven Molina <steven.molina.slayer@gmail.com>
2024-03-15 01:24:29 -0300

    allowed host

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ load_dotenv()
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
+ALLOWED_HOSTS = [os.environ.get('HOST')]
+
 # DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
